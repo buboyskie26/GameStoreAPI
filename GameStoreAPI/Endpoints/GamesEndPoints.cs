fe23a1799a3f49aaa246dca3c9eb99d668704a8c
Justine Sirios <justinesirios15@gmail.com>
2024-07-14 19:25:36 +0800

    Complete Get All

--- GameStoreAPI/Endpoints/GamesEndPoints.cs
+++ GameStoreAPI/Endpoints/GamesEndPoints.cs
@@ -45,6 +45,7 @@ public static class GamesEndPoints
         group.MapGet("/", async (IGamesRepository repository) =>
         {
             var test = string.Empty;
+            var test2 = string.Empty;
 
             return Results.Ok((await repository.GetAllAsync()));
 
